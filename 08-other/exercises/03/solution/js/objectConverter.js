daf25fbf64f56c03f0445334d6ceada379bd9280
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-08-30 16:28:22 +0200

    kész

--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,39 @@
+// 3. Készíts egy `objectConverter` nevű objektumot, ami az alábbi metódusokkal rendelkezik:
+//    - `arrayToMap(array)`
+//    - `arrayToSet(array)`
+//    - `setToMap(set)`
+//    - `setToArray(set)`
+//    - `mapToArray(map)`
+//    - `mapToSet(map)`
+
+// A fenti metódusok értelemszerűen nem másra szolgálnak, mint az egyik összetett adattípusból konvertálják át az adatokat egy másikba. 
+// Mindegyik metódus visszatérési értéke egy új Array, Map, vagy Set object.
+
+const objectConverter = {
+    arrayToMap(array) {
+        let simpleMap = new Map();
+        array.forEach((value, i) => simpleMap.set(i, value));
+        return simpleMap;
+    },
+    arrayToSet (array) {
+        return new Set(array);
+    },
+    setToMap(set) {
+        let simpleMap = new Map();
+        Array.from(set).forEach((value, i) =>  simpleMap.set(i, value));
+        return simpleMap;
+    },
+    setToArray(set) {
+        return Array.from(set);
+    },
+    mapToArray(map) {
+        return Array.from(map.values());
+    },
+    mapToSet(map) {
+        let simpleSet = new Set();
+        map.forEach((value) => simpleSet.add(value));
+        return simpleSet;
+    },
+}
+
 export default objectConverter;
--- 08-other/exercises/04/solution/js/customDateFormats.js
+++ 08-other/exercises/04/solution/js/customDateFormats.js
@@ -1,1 +1,25 @@
+// 4. Írj egy függvényt `customDateFormats` néven, ami paraméterként egy Date objektumot kap, a visszatérési értéke pedig egy objektum két tulajdonsággal, 
+// ami a következő formátumban tartalmazza a paraméterként kapott dátumot:
+// // - `short`: 2020. jan. 11. 14:20 (tehát évszám, a hónap neve röviden magyarul, kisbetűkkel a nap száma, majd az idő a másodperc nélkül)
+// - `long`: 2020. január 11. 14:20:10 (tehát évszám, a hónap neve magyarul, a nap száma, majd az idő)
+const customDateFormats= (date) => {
+    return {
+        'short': date.toLocaleDateString("hu-hu", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        'long': date.toLocaleDateString("hu-hu", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+      };
+} 
+
 export default customDateFormats;
